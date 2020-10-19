9c05162413a5e744f857ef8b5bd8020f3e198fc0
Edwart Sanchez <machiavelick22@gmail.com>
2020-10-19 04:28:15 -0500

    Formulario creado

---
+++ Controllers/FormularioController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Practica1.Models;
+
+namespace Practica1.Controllers
+{
+    public class FormularioController : Controller
+    {
+        private readonly ILogger<FormularioController> _logger;
+
+        public FormularioController(ILogger<FormularioController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+       [HttpPost]
+       public IActionResult Registrar(Formulario objFormulario){
+           objFormulario.Respuesta ="Datos registrados";
+           return View("index", objFormulario);
+       }
+    }
+}
+
+    
---
+++ Models/Formulario.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace Practica1.Models
+{
+    public class Formulario
+    {
+        public String Nombre { get; set; }
+
+        public String Apellido { get; set; }
+
+        public String Distrito { get; set; }
+
+        public  String Banco { get; set; }
+
+        public  int Edad { get; set; }
+
+        public String Genero { get; set; }
+
+        public String Respuesta { get; set; }
+
+
+
+    }
+}
